e728e7c973b25df3303e907a8e4d166d8348b08c
Dragos Bogdan <dragosbogdan@gmail.com>
2015-06-12 15:59:18 -0700

    includes build... again

---
+++ dist/scripts/scripts.ac9081ae.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("ngMadLibsApp",[]),angular.module("ngMadLibsApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
