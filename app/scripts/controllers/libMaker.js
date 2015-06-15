ba8c41a4e49e068cc9d844e7b0054a023e358c13
Dragos Bogdan <dragosbogdan@gmail.com>
2015-06-15 16:59:50 -0700

    adding a controller... not much else

---
+++ app/scripts/controllers/libMaker.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngMadLibsApp.controller:libMaker
+ * @description
+ * # libMaker
+ * Controller of the ngMadLibsApp
+ */
+angular.module('ngMadLibsApp')
+  .controller('libMaker', function ($scope) {
+
+  	
+
+  });
